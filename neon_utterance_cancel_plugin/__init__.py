dbe760ffe49aa232228671d3aa7fe22470f5ef04
jarbasai <jarbasai@mailfence.com>
2021-11-19 07:53:40 +0000

    pass context

--- neon_utterance_cancel_plugin/__init__.py
+++ neon_utterance_cancel_plugin/__init__.py
@@ -22,35 +22,33 @@
 # LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
 # NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 # SOFTWARE,  EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-from neon_transformers import UtteranceTransformer
 from os.path import join, dirname, isfile
+
 from ovos_utils.log import LOG
+
+from neon_transformers import UtteranceTransformer
 
 
 class Nevermind(UtteranceTransformer):
     def __init__(self, name="utterance_cancel", priority=15):
         super().__init__(name, priority)
-        lang = self.config.get("lang", "en-us")
+
+    @staticmethod
+    def get_cancel_words(lang="en-us"):
         res_path = join(dirname(__file__), "res", lang, "cancel.dialog")
         if isfile(res_path):
             with open(res_path) as f:
-                self.cancel_words = [l.strip() for l in f.read().split("\n")
-                                if l and not l.startswith("#")]
+                return [l.strip() for l in f.read().split("\n")
+                        if l and not l.startswith("#")]
         else:
             LOG.warning(f"cancel.dialog not available for {lang}")
-            self.cancel_words = []
+        return []
 
-    def transform(self, utterances, lang="en-us"):
-        for nevermind in self.cancel_words:
+    def transform(self, utterances, context):
+        lang = context.get("lang", "en-us")
+        for nevermind in self.get_cancel_words(lang):
             for utterance in utterances:
                 if utterance.endswith(nevermind):
                     return [], {"canceled": True, "cancel_word": nevermind}
 
         return utterances, {}
-
-
-def create_module():
-    return Nevermind()
-
-
-
